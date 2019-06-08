c922a82ffc14084daebe640de56be3e19a9935d3
40623153 <40623153@gm.nfu.edu.tw>
2019-06-09 00:38:57 +0800

    0609

--- v-rep/tablefootball.py
+++ v-rep/tablefootball.py
@@ -8,13 +8,13 @@ import keyboard
 vrep.simxFinish(-1)
   
 clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
-KickBallV = 360   
-#
+KickBallV = 360
+#揮棒速度
 Move_Minus =-0.1        
 Move_Plus =0.1
 n=1
 R_KickBallVel = (math.pi/180)*KickBallV
-#
+#揮棒後球速
 B_KickBallVel = -(math.pi/180)*KickBallV
 if clientID!= -1:
     print("Connected to remote server")
